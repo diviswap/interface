a6c12960a09870453c581a45149b30148ca6056e
marcersst <104516883+marcersst@users.noreply.github.com>
2024-07-27 00:14:53 -0300

    kewl list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -3,5 +3,6 @@ export const DEFAULT_TOKEN_LIST_URL = 'https://ipfs.io/ipfs/QmcDWGm4KEqytTk8Ppau
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  'https://ipfs.io/ipfs/QmcDWGm4KEqytTk8PpauJidkKwRDY38fk5gLjEfNVnFF6Z'
+  'https://ipfs.io/ipfs/QmcDWGm4KEqytTk8PpauJidkKwRDY38fk5gLjEfNVnFF6Z',
+  'https://ipfs.io/ipfs/QmPGPbdCmLH1DTBjVigL92A6Car7Sikb7ejdEaqN25p1XN'
 ]
