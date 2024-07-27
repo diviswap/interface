c61282482215dfe1a25c30051ad2b962eb999fd8
marcersst <104516883+marcersst@users.noreply.github.com>
2024-07-26 21:57:10 -0300

    svg KWL

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,7 +1,7 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://ipfs.io/ipfs/QmXQF7TwduyN5bBK2YftqzhPBL25bU1r6sPP2siBhoEbqR'
+export const DEFAULT_TOKEN_LIST_URL = 'https://ipfs.io/ipfs/QmUnhyaPenHDpwwUvejuzqGNXRavHbUcn7UKAgNaHKqiow'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  'https://ipfs.io/ipfs/QmXQF7TwduyN5bBK2YftqzhPBL25bU1r6sPP2siBhoEbqR'
+  'https://ipfs.io/ipfs/QmUnhyaPenHDpwwUvejuzqGNXRavHbUcn7UKAgNaHKqiow'
 ]
