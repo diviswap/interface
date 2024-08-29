b00677b4a5fc25332107f0d1f1e7f225b2960bec
diviswap <troyanovichd@icloud.com>
2024-08-29 17:08:59 -0300

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -3,6 +3,6 @@ export const DEFAULT_TOKEN_LIST_URL = 'https://ipfs.io/ipfs/QmcDWGm4KEqytTk8Ppau
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
-  'https://ipfs.io/ipfs/QmUQKxS1uMe9aShKYmgWZBCgyXbSKgaWahF5KQQeZyHm3L',
+  'https://ipfs.io/ipfs/QmcDWGm4KEqytTk8PpauJidkKwRDY38fk5gLjEfNVnFF6Z',
   'https://ipfs.io/ipfs/QmPGPbdCmLH1DTBjVigL92A6Car7Sikb7ejdEaqN25p1XN'
 ]
